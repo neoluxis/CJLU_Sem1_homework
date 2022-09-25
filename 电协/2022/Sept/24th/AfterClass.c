42b8a35a1c0354d5155b4fcff70f7e57da8aed97
neoluxis <neolux.lee@neolux.ga>
2022-09-25 14:01:27 +0800

    edited

--- 电协/2022/Sept/24th/AfterClass.c
+++ 电协/2022/Sept/24th/AfterClass.c
@@ -13,7 +13,7 @@
  *               *****
  *                ***
  *                 *
- * 	              ***  
+ * 	              ***
  *               *****
  *              *******
  *             *********
@@ -26,30 +26,41 @@
 
 int main(int argc, char **argv)
 {
+	// get the num of lines from user input
 	int n = 8;
 	printf("pls input n:\n");
 	scanf("%d", &n);
+
+	// gen a char array with '*' being the value of all elements according to n
+	// calculate the length of the array
 	int length = n * 2 - 1;
+	// gen array
 	char line[length + 1];
+	// set all elements to '*'
 	for (int c = 0; c < length; c++)
 	{
 		line[c] = '*';
 	}
+	// mark the end of the string
 	line[length] = '\0';
+	// print the string
 	printf("%s\n", line);
+
+	// printf the hourglass
 	for (int i = 0; i < length - 1; i++)
 	{
+		// thr upper
 		if (i < (n - 1))
 		{
 			line[i] = ' ';
 			line[length - i - 1] = ' ';
 			printf("%s\n", line);
 		}
+		// the mid
 		else if (i == n - 1)
 		{
-			// line[length / 2] = '*';
-			// printf("%s\n", line);
 		}
+		// the bottom
 		else
 		{
 			line[length - (i + 1)] = '*';
