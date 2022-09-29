9e6c32b934e4f3379155f311e765656471c8d951
neoluxis <neolux.lee@neolux.ga>
2022-09-29 19:49:07 +0800

    3.3

--- Clang/2022/Opt/work3/3.c
+++ Clang/2022/Opt/work3/3.c
@@ -1,19 +1,28 @@
 /**
  * @file 3.c
  * @author Neolux Lee (hello.neolux.ml)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-09-29
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 #include "stdio.h"
 
 int main(int argc, char const *argv[])
 {
-    int i=1, line=1;
-    
+    int i = 1, inLine=0,line = 1;
+    for (i; i <= 91; i++)
+    {
+        printf("%3d", i);
+        inLine++;
+        if(inLine==line){
+            printf("\n");
+            line++;
+            inLine = 0;
+        }
+    }
     return 0;
 }
