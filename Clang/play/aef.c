e39d72bdd3f0e396e968ad416a72b6868819f249
neoluxis <neolux.lee@neolux.ga>
2022-10-02 09:56:57 +0800

    kj

---
+++ Clang/play/aef.c
@@ -0,0 +1,21 @@
+/**
+ * @file aef.c
+ * @author Neolux (hello.neolux.ml)
+ * @brief
+ * @version 0.1
+ * @date 2022-10-02
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    scanf("%d", &a);
+    b = a != 0;
+    printf("%d\n", b);
+    return 0;
+}
