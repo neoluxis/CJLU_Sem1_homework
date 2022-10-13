0053aff6916c4c0272deab68f2ca8b6b509226b2
Neolux <neolux.lee@gmail.com>
2022-10-13 11:16:46 +0000

    update on Samsung Tab

---
+++ onSamsungTab/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../utils/dataConv.h"
+#include "../utils/tools.h"
+
+int main(){
+    printBin(12);
+    return 0;
+}
---
+++ utils/tools.h
@@ -0,0 +1,7 @@
+void sleep(unsigned int millis){
+    for(int i=0;i<millis;i++){
+        for(int j=0;j<1000;j++){
+            // sleep()
+        }
+    }
+}
